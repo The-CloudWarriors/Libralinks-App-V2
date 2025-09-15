979df0fa53e25c2a2f6b39ff5ef2f7bde83e96c5
lewisfung <ubuntu@ip-10-0-3-194.ec2.internal>
2025-09-15 01:29:24 +0000

    updated libralinks with .war to be be use with Tomcat 10-14-25

--- src/main/java/com/example/libraryfinder/LibraryFinderApplication.java
+++ src/main/java/com/example/libraryfinder/LibraryFinderApplication.java
@@ -2,10 +2,18 @@ package com.example.libraryfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryFinderApplication {
+public class LibraryFinderApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(LibraryFinderApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LibraryFinderApplication.class);
     }
 }
